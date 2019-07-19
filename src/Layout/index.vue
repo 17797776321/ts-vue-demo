441cc81715b6689e7830dfaf2bedc25a8a1e3e07
Chen <531336750@qq.com>
2019-07-19 11:19:37 +0800

    添加路由

---
+++ src/Layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout">
+    <router-view />
+  </div>
+</template>
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+@Component({
+  name: 'Layout',
+})
+export default class extends Vue {
+}
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Router, { RouteConfig } from 'vue-router';
+import Layout from '@/Layout/index.vue';
+Vue.use(Router);
+// 静态路由
+export const constantRoutes: RouteConfig[] = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/Home.vue'),
+      },
+    ],
+    meta: { title: '首页' },
+  },
+];
+// 动态挂在路由
+export const asyncRoutes: RouteConfig[] = [];
+
+const createRouter = () => new Router({
+  scrollBehavior: (to, from, savePosition) => {
+    if (savePosition) {
+      return savePosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
+  base: process.env.BASE_URL,
+  routes: constantRoutes,
+});
+const router = createRouter();
+export function resetRouter() {
+  const newRouter = createRouter();
+  (router as any).matcher = (newRouter as any).matcher;
+}
+
+export default router;
