727cc74bea33e24de398248d23fde332b998fdbd
Chen <531336750@qq.com>
2019-07-19 14:38:19 +0800

    引入SVG组件

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ import store from './store';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/style/global.less';
+// 引入SVG
+import SvgIcon from 'vue-svgicon';
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
-
+Vue.use(SvgIcon, {
+  tagName: 'svg-icon'
+});
 new Vue({
   router,
   render: h => h(App)
