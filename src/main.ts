cdf4d60ca45e66a5c6f3fe7c673bca1968890cad
Chen <531336750@qq.com>
2019-07-19 14:34:00 +0800

    添加vuex

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import store from './store';
 // 引入element
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/style/global.less';
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app');
