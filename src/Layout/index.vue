9378cb78dd773e5a64d7fd8d3678036f139dad22
Chen <531336750@qq.com>
2019-07-19 14:53:29 +0800

    布局

--- src/Layout/index.vue
+++ src/Layout/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="layout">
-    <a href="javascript:void(0);">111</a>
-    <router-view />
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
 <script lang="ts">
@@ -13,3 +22,23 @@ export default class extends Vue {
   private a: String = "aaa";
 }
 </script>
+<style lang="less">
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  height: 100vh;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+}
+</style>
