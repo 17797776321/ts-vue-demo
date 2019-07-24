4e2828a2a7c58ae67a942737fde284fd744a23a8
Chen <531336750@qq.com>
2019-07-24 17:23:24 +0800

    改个东西

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="page-login">{{pageName}}</div>
+  <div class="page-login">登录页</div>
 </template>
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
@@ -15,5 +15,6 @@ export default class Login extends Vue {
 .page-login {
   width: 100vw;
   height: 100vh;
+  background-color: #2d3a4b !important;
 }
 </style>
