d5e94edfbc96cc483dcf1880de88582cabd4444c
Chen <531336750@qq.com>
2019-08-07 14:56:31 +0800

    回退了

--- src/Layout/components/Menu.vue
+++ src/Layout/components/Menu.vue
@@ -11,27 +11,6 @@
       @close="handleClose"
       :collapse="!sidebar.isCollapse"
     >
-      <!-- <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
-        </template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-        <el-menu-item index="1-3">选项3</el-menu-item>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>-->
       <div v-for="(item,index) in routes" :key="index">
         <el-submenu
           v-if="(item.children && item.children.length>1) && (!item.meta || !item.meta.hidden)"
